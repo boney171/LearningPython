16fce19b6bd3e949f85ca2c565e29974e10553a7
boney171 <ttran456@ucr.edu>
2022-04-03 12:09:02 -0700

    Fixed the change_name function

--- CarClass.py
+++ CarClass.py
@@ -8,7 +8,7 @@ class Car:
      self.year=year
      self.odo=0
   def change_name(self,new_name):
-      self.name=new_name
+      self.make=new_name
   def change_model(self,new_model):
       self.model=new_model
   def print_out(self):
@@ -24,7 +24,7 @@ my_car.print_out()
 her_car = Car("Honda Civid", "XLE", 2021)
 her_car.print_out()
 
-my_car.change_name("tesla")
+my_car.change_name("Audi")
 my_car.print_out()
 my_car.set_odo(12321)
 my_car.print_out()
@@ -38,13 +38,13 @@ class ElectricCar(Car):
          super().__init__(make,model,year)
          self.battery=battery
     def print_out(self):
-     msg1=f"Car's make: {self.make}, Model: {self.model}, Year: {self.year}, Odo reading: {self.odo} \n This car has {self.odo} miles \n This car has {self.battery}% battery"
+     msg1=f"Car's make: {self.make}, Model: {self.model}, Year: {self.year}, Odo reading: {self.odo} \n This car has {self.odo} miles \n This car has {self.battery}% battery "
      print(msg1)
 my_tesla=ElectricCar("Tesla","Model 3",2022,100)
 my_tesla.set_odo(2320)
 my_tesla.print_out()
 my_tesla.change_name("Audi")
+my_tesla.change_name("A32")
 my_tesla.print_out()
-my_tesla.change_name("Hello")
-my_tesla.print_out()
+
 
